1870b8d7f4a72e05ac5e0f348dde200c488893fd
mrfrolk <ekaterita20@gmail.com>
2025-03-03 12:13:43 +0500

    init

---
+++ flutter_handle.ps1
@@ -0,0 +1,17 @@
+
+# после обновления пакета
+flutter pub cache clean
+flutter clean
+flutter pub get
+
+
+flutter pub add flutter_riverpod
+flutter pub add riverpod_annotation
+flutter pub add dev:riverpod_generator
+flutter pub add dev:build_runner
+flutter pub add dev:custom_lint
+flutter pub add dev:riverpod_lint
+flutter pub add hooks_riverpod
+
+# import 'package:flutter_riverpod/flutter_riverpod.dart';
+# import 'package:hooks_riverpod/hooks_riverpod.dart';
