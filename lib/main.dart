23a442c33889e2e5f688372552311c824484c762
mrfrolk <ekaterita20@gmail.com>
2025-03-03 17:46:31 +0500

    1

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,68 @@ import 'package:flutter/material.dart';
 import 'package:counter/extensions/context_extensions.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final counterProvider = StateProvider<int>((ref) => 10);
+// final counterProvider = StateProvider<int>((ref) => 10);
+// --------------------------------------------------------------
 // final counterProvider = Provider<int>((ref) => 10);
-final counterNotifierProvider = StateNotifierProvider<CounterNotifier, int>(
-  (ref) => CounterNotifier(),
-);
+// --------------------------------------------------------------
+// class CounterNotifier extends StateNotifier<int> {
+//   CounterNotifier() : super(10);
+//   void increment() => state++;
+//   void decrement() => state--;
+// }
 
-class CounterNotifier extends StateNotifier<int> {
-  CounterNotifier() : super(10);
+// final counterNotifierProvider = StateNotifierProvider<CounterNotifier, int>(
+//   (ref) => CounterNotifier(),
+// );
 
-  void increment() => state++;
-  void decrement() => state--;
+// --------------------------------------------------------------
+
+class Task {
+  final String taskName;
+  final bool isCompleted;
+  final DateTime dueDate;
+
+  Task({
+    required this.taskName,
+    required this.isCompleted,
+    required this.dueDate,
+  });
+
+  Task ourconstructor({
+    String? taskName,
+    bool? isCompleted,
+    DateTime? dueDate,
+  }) {
+    return Task(
+      taskName: taskName ?? this.taskName,
+      isCompleted: isCompleted ?? this.isCompleted,
+      dueDate: dueDate ?? this.dueDate,
+    );
+  }
 }
+
+class TaskNotifier extends StateNotifier<Task> {
+  TaskNotifier()
+    : super(
+        Task(taskName: "Задача 1", isCompleted: false, dueDate: DateTime(2025)),
+      );
+
+  void updateTask(String taskName) {
+    state = state.ourconstructor(taskName: taskName);
+  }
+
+  void updateCompleteStatus(bool isCompleted) {
+    state = state.ourconstructor(isCompleted: isCompleted);
+  }
+
+  void updateDate(DateTime dueDate) {
+    state = state.ourconstructor(dueDate: dueDate);
+  }
+}
+
+final taskNotifierProvider = StateNotifierProvider<TaskNotifier, Task>((ref) {
+  return TaskNotifier();
+});
 
 void main() {
   runApp(ProviderScope(child: App()));
@@ -33,7 +83,7 @@ class Home extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final counter = ref.watch(counterNotifierProvider);
+    final task = ref.watch(taskNotifierProvider);
 
     return Scaffold(
       body: Center(
@@ -46,10 +96,10 @@ class Home extends ConsumerWidget {
             children: [
               Text("Количество нажатий:", style: TextStyle(fontSize: 15)),
               SizedBox(height: 30),
-              Text('$counter', style: TextStyle(fontSize: 25)),
+              Text("${task.dueDate}", style: TextStyle(fontSize: 25)),
               SizedBox(height: 30),
               ElevatedButton(
-                onPressed: () => ref.read(counterNotifierProvider.notifier),
+                onPressed: () => ref.read(taskNotifierProvider.notifier).updateDate(DateTime.now()),
                 child: Text("Увеличить", style: TextStyle(fontSize: 15)),
               ),
 
